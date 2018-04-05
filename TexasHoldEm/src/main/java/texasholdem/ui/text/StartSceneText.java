40cbda5061491452c2bca11ae380b0e9f38c2424
Joonas <joonas.suortti@helsinki.fi>
2018-04-05 21:03:26 +0300

    harjoitustyö aloitettu

---
+++ TexasHoldEm/src/main/java/texasholdem/ui/text/StartSceneText.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texasholdem.ui.text;
+
+/**
+ *
+ * @author josujosu
+ */
+import texasholdem.ui.Scene;
+
+public class StartSceneText implements Scene{
+    
+}
